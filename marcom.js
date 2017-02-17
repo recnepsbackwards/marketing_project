6d1191bb6ba0323074eab395aab1a8bc3e7f8b14
Spencer <spencer@postings.com>
2017-02-17 11:26:04 -0800

    custom stylesheet

---
+++ marcom.js
@@ -0,0 +1,4 @@
+$( document ).ready(function() {
+  var nav = '<div style="width:100%;height:auto;background-image:url(http://images.assetmarketingsystems.com:8088/broadcastimages/BroadcastAPP/marcom/arrows-bg.png);background-color:#1dade4;  padding: 20px 0px;"><div id="new-footer" style="color:#ffffff;padding: 7px 20px 7px 20px;margin: 0px auto;">&copy;Asset Marketing Systems 2017 - All rights reserved - 888-303-8755 - <a style="color:#ffffff;" href="mailto:info@assetismarketing.com"><u>info@assetismarketing.com</u></a></div></div>';
+  $('#insert-nav').append(nav);
+});
