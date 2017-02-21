4d777e7a9dc8a5a9d338268404e6840768769703
Spencer <spencer@postings.com>
2017-02-21 13:43:00 -0800

    adds consultant.js

---
+++ consultant.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  //Sets variable contact to block of code which has in-line styling.
+  var contact = '<div class="text-center contact-wrapper"><h1>Have a question?</h1><h3>Get in touch!</h3><div class="contact-information"><div><a href="http://info.amsfmo.com/marcom-standard" target="_blank"><button type="button" name="button" class="button">Contact A Consultant</button></a></div></div></div>'
+
+  //Appends block dynamically to page.
+  $('#insert-cta').append(contact);
+
+});
--- marcom.js
+++ marcom.js
@@ -5,18 +5,8 @@ $(document).ready(function() {
   //Appends block dynamically to page.
   $('#insert-footer').append(footer);
 
-  //Sets variable contact to block of code which has in-line styling.
-  var contact = '<div class="text-center contact-wrapper"><h1>Have a question?</h1><h3>Get in touch!</h3><div class="contact-information"><div><a href="http://info.amsfmo.com/marcom-standard" target="_blank"><button type="button" name="button" class="button">Contact A Consultant</button></a></div></div></div>'
-
-  //Appends block dynamically to page.
-  $('#insert-contact').append(contact);
-
   //Removes PTI copyright from footer
   $('#copyright').html('');
-
-  var logo = '<li style="white-space:nowrap;" class=" navBarEnhancedBackgroundColor customColorOverridable navBarItem"><a class="navBarEnhancedLinkColor customColorOverridable" href="https://marcomcentral.app.pti.com/Asset_Marketing_Systems/assetmarketing/home.aspx?uigroup_id=478856"><img src="https://images.printable.com/printonelogos/images/3015/20925/banners/478856/asset-logo.png" alt=""></a></li>';
-
-  $('.catalogRoot').append(logo);
 
   //------Function to cycle through images in Ad banner-----
   //Sets the Index to 0
@@ -45,7 +35,7 @@ $(document).ready(function() {
           theAd = 0;
        }
        //Change the image src based on the Array
-       //banner-ad.src = Array[0] (image source of index 0)
+       //rotate-image.src = Array[0] (image source of index 0)
       $('.rotate-image img').attr('src', adImages[theAd]);
       //Sets a timeout for the rotation
        setTimeout(rotate, 3 * 1000);
