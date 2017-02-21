68adb18f186e03d9af544220a6eb129a770ed738
Spencer <spencer@postings.com>
2017-02-21 10:55:22 -0800

    removes unused files

--- marcom.js
+++ marcom.js
@@ -14,6 +14,10 @@ $(document).ready(function() {
   //Removes PTI copyright from footer
   $('#copyright').html('');
 
+  var logo = '<li style="white-space:nowrap;" class=" navBarEnhancedBackgroundColor customColorOverridable navBarItem"><a class="navBarEnhancedLinkColor customColorOverridable" href="https://marcomcentral.app.pti.com/Asset_Marketing_Systems/assetmarketing/home.aspx?uigroup_id=478856"><img src="https://images.printable.com/printonelogos/images/3015/20925/banners/478856/asset-logo.png" alt=""></a></li>';
+
+  $('.catalogRoot').append(logo);
+
   //------Function to cycle through images in Ad banner-----
   //Sets the Index to 0
   var theAd = 0;
--- nav.js
+++ nav.js
@@ -1,4 +1,6 @@
-prepend
-https://images.printable.com/printonelogos/images/3015/20925/banners/478856/asset-logo.png
+$(document).ready(function() {
+  var logo = '<li style="white-space:nowrap;" class=" navBarEnhancedBackgroundColor customColorOverridable navBarItem"><a class="navBarEnhancedLinkColor customColorOverridable" href="https://marcomcentral.app.pti.com/Asset_Marketing_Systems/assetmarketing/home.aspx?uigroup_id=478856"><img src="https://images.printable.com/printonelogos/images/3015/20925/banners/478856/asset-logo.png" alt=""></a></li>';
 
-http://jobs.ondrejsvestka.cz/1-2/
+  $('.catalogRoot').append(logo);
+
+});
